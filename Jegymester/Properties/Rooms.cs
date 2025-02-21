9bdb7c4d073bc6d4f5c1795ddee0c1a781952da8
Kornél Németh <S92Q5R@student.uni-pannon.hu>
2025-02-22 00:14:08 +0100

    Székek kezelése

--- Jegymester/Properties/Rooms.cs
+++ Jegymester/Properties/Rooms.cs
@@ -1,8 +1,47 @@
-﻿namespace Jegymester.Properties
+﻿using System.Runtime.CompilerServices;
+namespace Jegymester.Properties
 {
     public class Rooms
     {
-        public List<int> Chairs = new List<int>(100);
-        
+        // szobában levő székek kezelése
+        public List<bool> Chairs = new List<bool>(100); // 10x10 terem
+        public void InitializingChairs()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Chairs.Add(false); // ha üres: 0
+            }
+        }
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void BookChair(int chairToBeBooked) // a kapott érték eggyel igazított
+        {
+            if(Chairs.Any(c => c == false))
+            {
+                Console.WriteLine("a teremben nincsen ütes hely");
+            }
+            // szabad e a kiválasztott szék
+            if (!Chairs[chairToBeBooked])
+            {
+                Chairs[chairToBeBooked] = true;
+                Console.WriteLine("sikeres jegyfoglalás"); 
+            }
+            Console.WriteLine("hiba"); 
+        }
+        public string GetEmptyChairs()
+        {
+            string emptyChairsString = "Szabad székek: ";
+            for (int i = 0; i < 99; i++)
+            {
+                if (!Chairs[i])
+                {
+                    int dim1 = 0; //kisebb szám
+                    double dim2 = 0; // 10re osztva
+                    dim1 = (i % 10) + 1;
+                    dim2 = Math.Floor((double)i / 10);
+                    emptyChairsString += $"{dim2} sor, {dim1} oszlop";
+                }
+            }
+            return emptyChairsString;
+        }
     }
 }
