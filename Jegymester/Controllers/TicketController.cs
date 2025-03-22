1b5ac9b068d05fc8c4b759415af734ceb95ee9e3
Matyi <kiraly.matyas66@gmail.com>
2025-03-22 12:40:20 +0100

    - Class library for services and the automapper - Folder for dtos

---
+++ Jegymester/Controllers/TicketController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Jegymester.Entities;
+using Jegymester.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jegymester.Controllers
+{
+    [Route("api/Ticket")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        JegymesterDbContext _DbContext;
+
+        // Ticket letrehozas
+
+        // Ticket torles
+
+        // Ticket lekeres
+
+    }
+}
