34cc8f688d3d6e49f4919dc9d7f55b67b60cb274
Nyirő Teofil <103335721+nyiroteofil@users.noreply.github.com>
2025-03-28 09:16:01 +0100

    -MovieDto implemented -IMovieService file created, but not implemented

---
+++ Entities/Dtos/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jegymester.DataContext.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public string MovieDescrition { get; set; }
+    }
+
+    public class MovieUpdateDto
+    {
+        public string MovieName { get; set; }
+        public string MovieDescrition { get; set; }
+    }
+
+    public class MovieCreateDto
+    {
+        public string MovieName { get; set; }
+        public string MovieDescrition { get; set; }
+    }
+}
---
+++ Jegymester.ServicesA/IMovieService.cs
@@ -0,0 +1,46 @@
+﻿using Jegymester.DataContext.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jegymester.Services
+{
+    public interface IMovieService
+    {
+        Task<MovieDto> GetMovieByIdAsync(int movieId);
+        Task<IEnumerable<MovieDto>> GetAllMoviesOnScreenengAsync();
+        Task<bool> UpdateMovieAsync(MovieDto dto);
+        Task<bool> DeleteMovieAsync(int movieId);
+        Task<bool> UpdateMovieAsync(MovieUpdateDto dto);
+    }
+
+    public class MovieService : IMovieService
+    {
+        public Task<bool> DeleteMovieAsync(int movieId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MovieDto>> GetAllMoviesOnScreenengAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MovieDto> GetMovieByIdAsync(int movieId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateMovieAsync(MovieDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateMovieAsync(MovieUpdateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
